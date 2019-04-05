98c7f8698d32315227cbbabf4a957b4265b5d29d
Glendy <glendyco@gmail.com>
2019-04-05 02:53:51 -0600

    Pruebas Unitarias Login - 201025406

---
+++ SB2/SB2.Tests/Models/ConsultaTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SB2.Models;
+
+namespace SB2.Tests.Models
+{
+    [TestClass]
+    public class ConsultaTest
+    {
+        [TestMethod]
+        public void Login()
+        {
+            string user = "glendyco";
+            string pass = "hola123";
+         
+            // Arrange
+            Consulta controller = new Consulta();
+
+            // Act
+            Usuario result = controller.Login(user,pass) as Usuario;
+
+            // Assert
+            Assert.IsNotNull(result);
+         
+        }
+    }
+}
