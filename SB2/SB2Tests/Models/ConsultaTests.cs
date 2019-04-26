5a2492a6e407e6d8f02147de1ca668d42b925b64
glendyco <glendyco@gmail.com>
2019-04-26 00:49:35 -0600

    Nuget error v1 201025406

--- SB2/SB2Tests/Models/ConsultaTests.cs
+++ SB2/SB2Tests/Models/ConsultaTests.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SB2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SB2Tests.Models
+namespace SB2.Models.Tests
 {
-    class ConsultaTests
+    [TestClass()]
+    public class ConsultaTests
     {
+        [TestMethod()]
+        public void LoginTest()
+        {
+            Assert.Fail();
+        }
     }
 }
